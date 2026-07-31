33bf7b2e76ff4fbcb2de6ada7d07127175eeac37
Dauris11 <daurissantana1@gmail.com>
2026-07-31 09:30:50 -0400

    fix: corregir ruta de importación de PageHeader en servicios-mensuales

--- src/app/[locale]/(portal)/servicios-mensuales/page.tsx
+++ src/app/[locale]/(portal)/servicios-mensuales/page.tsx
@@ -1,5 +1,5 @@
-
-import { PageHeader } from "@/components/layout/page-header";
+import React from "react";
+import { PageHeader } from "@/components/ui/page-header";
 import { ServiciosMensualesClient } from "./servicios-mensuales-client";
 
 export default function ServiciosMensualesPage() {
